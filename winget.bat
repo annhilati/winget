b3acae8cf549cbc258ecc1b54aa7f658f18386ae
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-02-18 10:46:52 +0100

    Update winget.bat

--- winget.bat
+++ winget.bat
@@ -1,12 +1,6 @@
-@echo off
-# echo  __          _______ _   _  _____ ______ _______ 
-# echo  \ \        / /_   _| \ | |/ ____|  ____|__   __|
-# echo   \ \  /\  / /  | | |  \| | |  __| |__     | |   
-# echo    \ \/  \/ /   | | | . ` | | |_ |  __|    | |   
-# echo     \  /\  /   _| |_| |\  | |__| | |____   | |   
-# echo      \/  \/   |_____|_| \_|\_____|______|  |_| 
+@echo off 
 echo Please execute as administrator
 winget list
 winget upgrade --all
-echo If nothing happens after this message, please execute shell as administrator
+echo If nothing happens after this message and nothing has happened yet, please execute shell as administrator
 PAUSE
